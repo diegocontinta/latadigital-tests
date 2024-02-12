f4cee6693d8d031de3bbe6809cb0890bb664a47d
Diego A. Villarroel <hola@diegocontinta.com>
2024-02-12 19:45:06 -0300

    added final bubble sort algorithms --exercise-03-algorithms

--- exercise-03/order_bubble_sort.php
+++ exercise-03/order_bubble_sort.php
@@ -1,35 +1,22 @@
 <?php
 
-/** ... working
- * @param $array
- * @return void
+/** Retrieve ordered array base on array of integer numbers
+ * @param {array} $array int
+ * @return array
  */
-function bubbleSortOrder($array):void {
+function bubbleSortOrder($array):array {
 
-    $sort = function () use(&$array) {
-
-                 print_r($array);
-                 $iterate = true;
-                 $len = count($array) - 1;
-
-                 foreach ($array as $index => $item) {
-                     $a = $item;
-                     $b = $index < $len ? $array[$index + 1] : $item;
-                     if($a < $b)
-                         continue;
+        $iterate = true;
+        while ($iterate) {
+            $iterate = false;
+            foreach ($array as $index => $item) {
+                if (isset($array[$index + 1]) && $item > $array[$index + 1]) {
+                    $temp = $item;
+                    $array[$index] = $array[$index + 1];
+                    $array[$index + 1] = $temp;
+                    $iterate = true;
                 }
-
-                 print_r($array);
-
-            };
-
-
-    $sort();
-
-
+            }
+        }
+        return $array;
 }
-
-
-$array = [18,14,125,8,9,152,56,5];
-
-bubbleSortOrder($array);
